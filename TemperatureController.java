4404e46151cb4ff3528bb2922ae98d3e6f2db63a
Cdp11703 <137389943+Cdp11703@users.noreply.github.com>
2023-07-15 18:54:23 +0800

    Add files via upload

--- TemperatureController.java
+++ TemperatureController.java
@@ -1,7 +1,6 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
 public class TemperatureController {
 	
 	private Temperature tempM;
@@ -14,16 +13,39 @@ public class TemperatureController {
 		this.tempV.setConvertBtnListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				String celcius = tempV.getCelcius();
+				String fahrenheit = tempV.getFahrenheit();
+				
+				if (!celcius.isEmpty() && fahrenheit.isEmpty()) {
+					Double Celcius = Double.parseDouble(celcius);
+					Double Fahrenheit = (Celcius * 9/5) + 32.0;
+
+					tempM.setCelcius(Celcius);
+					tempM.setFahrenheit(Fahrenheit);
+
+					tempV.setInfo(Celcius + " Celsius to " + Fahrenheit + " Fahrenheit");
+
+				} else if (celcius.isEmpty() && !fahrenheit.isEmpty()) {
+					Double Fahrenheit = Double.parseDouble(fahrenheit);
+					Double Celcius = (Fahrenheit - 32.0) * 5/9;
+
+					tempM.setCelcius(Celcius);
+					tempM.setFahrenheit(Fahrenheit);
+
+					tempV.setInfo(Fahrenheit + " Fahrenheit to " + Celcius + " Celsius");
+
+				} else {
+					tempV.setInfo("Please input either temperature");
+				}
+
+				tempV.clearTextFields();
 			}
 		});
-
-
 	}
 
-	public void main(){
-		
+	public static void main(String args[]){
+		Temperature TempModel = new Temperature();
+		TemperatureView  TempView = new TemperatureView();
+		TemperatureController TempController = new TemperatureController(TempModel, TempView);
 	}
-		
-		
-		
 }
--- TemperatureView.java
+++ TemperatureView.java
@@ -2,10 +2,9 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
-import javax.swing.JTextArea;
 import javax.swing.JPanel;
 
-import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,16 +12,46 @@ import java.awt.event.ActionListener;
 public class TemperatureView {
 
     private JFrame frame;
-	private JLabel celciusLabel, fahrenheitLabel;
-	private JTextField celciusText, fahrenheitText, infoText;
-	private JButton convertBtn;
+    private JLabel celciusLabel, fahrenheitLabel;
+    private JTextField celciusText, fahrenheitText, infoText;
+    private JButton convertBtn;
 
-    public TemperatureView(){
+    public TemperatureView() {
         this.frame = new JFrame("Converter");
 
-		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.frame.setLayout(new FlowLayout(FlowLayout.CENTER));
-		this.frame.setSize(250, 400);
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.frame.setLayout(new GridLayout(4, 1));
+        this.frame.setResizable(false);
+        this.frame.setSize(400, 250);
+
+        this.celciusLabel = new JLabel("Celcius: ");
+        this.fahrenheitLabel = new JLabel("Fahrenheit: ");
+
+        this.celciusText = new JTextField();
+        this.celciusText.setColumns(10);
+        this.fahrenheitText = new JTextField();
+        this.fahrenheitText.setColumns(10);
+
+        this.convertBtn = new JButton("Convert");
+        this.convertBtn.setPreferredSize(new Dimension(220, 30));
+
+        this.infoText = new JTextField();
+        this.infoText.setEditable(false);
+
+        JPanel panel1 = new JPanel();
+        panel1.add(this.celciusLabel);
+        panel1.add(this.celciusText);
+        panel1.setPreferredSize(new Dimension(220, 30));
+
+        JPanel panel2 = new JPanel();
+        panel2.add(this.fahrenheitLabel);
+        panel2.add(this.fahrenheitText);
+        panel1.setPreferredSize(new Dimension(220, 30));
+
+        this.frame.add(panel1);
+        this.frame.add(panel2);
+        this.frame.add(this.convertBtn);
+        this.frame.add(this.infoText);
 
         this.frame.setVisible(true);
     }
@@ -31,24 +60,20 @@ public class TemperatureView {
         this.convertBtn.addActionListener(actn);
     }
 
-    public String getCelcius(){
+    public String getCelcius() {
         return this.celciusText.getText();
     }
 
-    public String getFahrenheit(){
+    public String getFahrenheit() {
         return this.fahrenheitText.getText();
     }
 
-    public void setInfo(String text){
+    public void setInfo(String text) {
         this.infoText.setText(text);
     }
 
     public void clearTextFields() {
-		this.celciusText.setText("");
-		this.fahrenheitText.setText("");
-	}
-
-
-
-
+        this.celciusText.setText("");
+        this.fahrenheitText.setText("");
+    }
 }
